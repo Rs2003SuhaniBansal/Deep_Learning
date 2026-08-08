000591a9cb4850b85074ad9d8626d7c614af2f86
Suhani Bansal <rs.suhanibansal@gmail.com>
2026-08-08 14:15:51 +0530

    modified

--- Lab2/network_1.py
+++ Lab2/network_1.py
@@ -1,9 +1,26 @@
 import numpy as np
 
-#Using Sigmoid function
 x = np.random.randn(4,1)
 w = np.random.randn(1,4)
 b = np.zeros((1,1))
+
+Z_11 = []
+for j in range(len(w)):
+    s = 0
+    for i in range(len(x)):
+        s += w[j][i]*x[i][0]
+    Z_11.append(s)
+
+Z_11 = Z_11 + b
+print("Z_11:",Z_11)
+
+print("----------------------")
+print("Using Numpy")
+
+a = np.maximum(0,Z_11)
+print("a:",a)
+
+# using numpy
 z = np.dot(w,x) + b
 print("Z:",z)
 a = np.maximum(0,z)
--- Lab2/nework_2.py
+++ Lab2/nework_2.py
@@ -8,23 +8,34 @@ b1 = np.zeros((3,1))
 b2 = np.zeros((2,1))
 b3 = np.zeros((1,1))
 
+
+# using for loops
+Z = []
+for i in range(len(w_1)):
+    s = 0
+    for j in range(len(x)):
+        s += w_1[i][j] * x[j][0]
+    Z.append(s)
+
+print("Z:",Z)
+
 # Using Relu
 print("layer1")
 z = np.dot(w_1,x) + b1
-
+print("z:", z)
 a = np.maximum(0,z)
 #for sigmoid function
 # a = 1/(1+(np.exp(-z)))
 print("a:",a)
 x = a
-
+print("---------------------")
 print("layer2")
 z = np.dot(w_2,x) + b2
 a = np.maximum(0,z)
 # a = 1/(1+(np.exp(-z)))
 print("a:",a)
 x = a
-
+print("---------------------")
 print("output layer")
 z = np.dot(w_3,x) + b3
 a = np.maximum(0,z)
