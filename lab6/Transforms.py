af0f83da699deadddf9759a105c6bb363d95f311
Suhani Bansal <rs.suhanibansal@gmail.com>
2026-08-25 17:18:03 +0530

    lab6

---
+++ lab6/Transforms.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn.functional as F
+from torchvision import datasets
+from torchvision.transforms import v2
+
+ds = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]),
+    target_transform=v2.Lambda(
+        lambda y: F.one_hot(torch.tensor(y), num_classes=10).float()
+    ),
+)
+
+target_transform = v2.Lambda(
+    lambda y: F.one_hot(torch.tensor(y), num_classes=10).float()
+)
+
